8dbe8827371f2ea760faf7203be23aaef6e482b1
Ramiro <ramirovazquez95@gmail.com>
2017-11-06 02:39:40 -0600

    algo

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -3,11 +3,22 @@ import './Playlist.css';
 import TrackList from '../TrackList/TrackList';
 
 class Playlist extends Component {
+
+  constructor(props){
+    super(props);
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  handleNameChange(e){
+    const name = e.target.value;
+    this.props.onNameChange(name);
+  }
+
   render() {
     return (
 
             <div className="Playlist">
-              <input defaultValue={'New Playlist'} />
+              <input defaultValue={'New Playlist'} onChange={this.handleNameChange} />
                 <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={this.props.isRemoval} />
               <a className="Playlist-save">SAVE TO SPOTIFY</a>
             </div>
